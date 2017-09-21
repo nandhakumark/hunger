56e149961d71e9916060acaf58c1e1efd3c16bda
nandha <knandhu1987@gmail.com>
2017-09-21 11:56:50 -0400

    Merged Login Component

--- HungerApp/src/app/app.module.ts
+++ HungerApp/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu_categories/menu.component';
 import { CartComponent } from './cart_page/cart.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [AppComponent, MenuComponent, CartComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [BrowserModule, FormsModule, HttpModule ],
+  declarations: [AppComponent,
+      MenuComponent,
+      CartComponent,
+      LoginComponent],
+  bootstrap: [AppComponent,
+      LoginComponent]
 })
 export class AppModule { }
